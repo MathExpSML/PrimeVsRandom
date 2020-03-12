078b2e0789b775ea3a03130eef5e7441707b6a20
Seb <plaaschou@gmail.com>
2020-03-12 09:59:25 +0100

    adding error plot code

--- Seb_R/xlogx.R
+++ Seb_R/xlogx.R
@@ -6,10 +6,16 @@ library(ggplot2)
 
 fpi <- function(n) length(generate_primes(2,n))
 
-x_logx <- tibble(x=seq(2,100),prime_cnt = sapply(x,FUN=fpi), xlog = x/log(x)) %>% 
+x_logx <- tibble(x=seq(2,10^5),prime_cnt = sapply(x,FUN=fpi), xlog = x/log(x)) %>% 
   ggplot() + 
   geom_line(aes(x,xlog), color = "red") + 
   geom_line(aes(x,prime_cnt), color = "blue") + 
   theme_minimal()
 
-ggsave("x_log.pdf",path="/Seb_R")
+error_xlogx <- N %>% 
+  ggplot() + 
+  geom_line(aes(x,abs(xlog-prime_cnt)), color = "red") +
+  theme_minimal()
+
+ggsave("x_log.pdf",path="Seb_R")
+ggsave("error_x_log.pdf",path="Seb_R")
