1f7b99f85eee9ee95a8da7f6d004689b562f6a23
Seb <plaaschou@gmail.com>
2020-03-18 17:12:27 +0000

    commented multi_sample

--- r_code/multi_sample.R
+++ r_code/multi_sample.R
@@ -1,37 +1,46 @@
+#import mods
+library(primes)
+library(magrittr)
+library(ggplot2)
+
+#generate a lis of primes up to 10^7
 primes <- generate_primes(2,10^7)
-length(primes)
+
+#create the bounds of my samples.
 x <- seq(2,20000)
 
-Pi <- function(x){
-  length(primes[which(primes <= x)])
+#define pi(x) function and generate a dataframe 
+Pi <- function(k){
+  length(primes[which(primes <= k)])
 }
 pi_df <- data.frame(x, c = sapply(x,FUN=Pi))
 
+#define function to generate a random sample
 test <- function(){
   d <- data.frame(x,v= sapply(x, logSel))
   d$x[which(d$v == TRUE)]
 }
 
-test_output <- replicate(100,test())
+# create multiple random sets 
+rand_sets <- replicate(100,test())
 
-count_i <- function(i,j) {length(which(test_output[[j]] <= i))}
+# define a function to count number of elements in random sample prior to X. We could
+# call this rho(x)
+Rho <- function(i,j) {length(which(rand_sets[[j]] <= i))}
 
-i = seq(1,3)
+# Generate a data frame to hold rho(x) for all samples
+holding_df <- data.frame(sapply(1:100,function(y) sapply(1:20000,Rho,y)))
 
-binding_table <- function(x,y){
-  sapply(x,)
-}
-
-d <- data.frame(sapply(1:100,function(y) sapply(1:20000,count_i,y)))
-test_output[1:3]
-
-dd <- data.frame()
-mapply(1:25,1:25,FUN=function(x,y) {
-  d2 <- data.frame(n = 1:20000, c = d[,x], label = paste("sample",y,sep="_"))
-  dd <<- rbind(dd,d2)
-}
+# Generate a binded data frame, in order to plot each random set differently.
+plot_size = 25   # number of samples to plot
+plot_df <- data.frame() #generate generic data.frame
+mapply(1:plot_size,1:plot_size,FUN=function(x,y) {
+  d <- data.frame(n = 1:20000, c = holding_df[,x], label = paste("sample",y,sep="_"))
+  plot_df <<- rbind(plot_df,d)
+  }
 )
 
+#Plot everything and save it in images/
 ggplot() + 
   geom_line(data = dd, aes(n,c, color=label),alpha = .4) +
   geom_line(data = pi_df, aes(x,c), color = "red",alpha = 1, linetype="dashed") +
