8ae0b61d7e86f9d65092f48179b2d44f9bb4a17b
Seb <plaaschou@gmail.com>
2020-03-18 10:04:34 +0000

    general comparison

--- r_code/general_comparison.R
+++ r_code/general_comparison.R
@@ -4,8 +4,8 @@
 li <- function(x){ expint_Ei(log(x))}
 xlog <- function(x){ x/log(x)}
 pi_x <- function(x){length(generate_primes(min=2, x))}
-Rsel <- function(x) {runif(1) <= li(x)/x}
-Rsel2 <- function(x) {runif(1) <= 1/log(x)}
+PiSel <- function(x) {runif(1) <= pi_x(x)/x}
+logSel <- function(x) {runif(1) <= 1/log(x)}
 RCount <- function(df, x){
   sum(df[1:x])
 }
@@ -13,21 +13,27 @@ RCount <- function(df, x){
 lbound <- 2
 bound <- 100000
 x <- seq(lbound,bound)
+
+Pi_tot <- sapply(x,PiSel) 
+Log_tot <- sapply(x, logSel)
+
 lix <- data.frame(x, y = sapply(x, li), label = "li(x)")
 xlogx <- data.frame(x, y = sapply(x,xlog), label = "x/log(x)")
 pix <- data.frame(x, y = sapply(x,pi_x), label = "pi(x)")
-R_tot <- sapply(x,Rsel) 
-R_tot2 <- sapply(x, Rsel2)
-Rcount <- data.frame(x, y = sapply(x,RCount,df = R_tot),label="random using li(x)/x")
-Rcount2 <- data.frame(x, y = sapply(x,RCount,df = R_tot2),label="random using 1/log(x)")
+Rcount <- data.frame(x, y = sapply(x,RCount,df = Pi_tot),label="random using Pi(x)/x")
+Log_Distrib <- data.frame(x, y = sapply(x,RCount,df = Log_tot),label="random using 1/log(x)")
 
 df <- rbind(
   pix,
   lix,
-  xlogx,
-  Rcount, 
-  Rcount2
+  #xlogx,
+  #Rcount, 
+  Log_Distrib
 )                  
-df %>% filter(x >= 0) %>% ggplot() + geom_line(aes(x,y,color = label)) + 
-  theme_minimal()
+df %>% ggplot() +
+  geom_line(aes(x,y,color = label)) + 
+  theme_minimal() + 
+  theme(legend.position="bottom") +
+  labs(color="function")
+
 ggsave("comparison.pdf",path="images")
